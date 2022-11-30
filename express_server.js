fa42a199c799a5726ef37693b5eb09080eac6403
Donnell Harris <donnell.harris@koodomobile.com>
2022-11-29 17:22:53 -0800

    completed login/logout feature

--- express_server.js
+++ express_server.js
@@ -2,20 +2,22 @@ const express = require("express");
 const { url } = require("inspector");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const cookieParser = require("cookie-parser");
 // set view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+
+// read cookies
+app.use(cookieParser());
 
 let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-
 
 const generateRandUrl = () => {
   const randUrl = Math.random().toString(36).slice(2, 8);
   return randUrl;
-}
+};
 
 // console.log(generateRandUrl());
 // middleware (middle man) -> sets encoding
@@ -24,20 +26,25 @@ app.use(express.urlencoded({ extended: true }));
 //use res.render to load up ejs view file
 
 // ROOT PAGE //
-app.get("/", (req, res) => {
-  templateVars = {
-    main: 'Drip Drop'
-  }
+// app.get("/", (req, res) => {
+//   templateVars = {
+//     main: "Drip Drop",
+//   };
 
-  res.render('dripDrop', templateVars);
-});
+//   res.render("dripDrop", templateVars);
+// });
 
 app.get("/urls", (req, res) => {
+  const username = req.body.username;
+
   const templateVars = {
-    urls: urlDatabase
+    urls: urlDatabase,
+    username: req.cookies['username'],
   };
 
-  res.render('urls_index', templateVars);
+  console.log('res cookies: ',res.cookies);
+  console.log('req cookies: ', req.cookies);
+  res.render("urls_index", templateVars);
 });
 
 //  CREATE NEW SHORT URL //
@@ -45,72 +52,93 @@ app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   let id = generateRandUrl();
   urlDatabase[id] = req.body.longURL;
-  
+
   console.log(urlDatabase);
   return res.redirect(`/urls/${id}`);
 });
 
-// DELETE URLS //
-app.post('/urls/:id/delete', (req, res) => {
-  const id = req.params.id
-  console.log(`Deleting shortURL: ${id} ==> ${urlDatabase[id]}`);
-  delete urlDatabase[id];
-  return res.redirect('/urls');
-})
-
 // EDIT EXISTING URL
-app.post('/urls/:id', (req, res) => {
+app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const newURL = req.body.editURL
-  
+  const newURL = req.body.editURL;
+
   if (urlDatabase.hasOwnProperty(id)) {
     urlDatabase[id] = newURL;
     console.log(`URL successfully changed`);
-
   }
-  res.redirect('/urls')
-})
+  res.redirect("/urls");
+});
+
+// LOG IN //
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  // console.log(username);
+  
+  res.cookie('username', username);
+  return res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  const username = req.body.username;
+
+  
+  res.clearCookie('username', username);
+  return res.redirect("/urls");
+});
+
+// DELETE URLS //
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  console.log(`Deleting shortURL: ${id} ==> ${urlDatabase[id]}`);
+  delete urlDatabase[id];
+  return res.redirect("/urls");
+});
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   const longURL = urlDatabase[id];
   res.redirect(longURL);
 });
 
 app.get("/about", (req, res) => {
-  res.render('pages/about');
+  res.render("pages/about");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/dripDrop', (req, res) => {
-  templateVars = {
-    main: 'Drip Drop'
-  }
-  
-  res.render('dripDrop', templateVars);
-});
+// app.get("/dripDrop", (req, res) => {
+//   templateVars = {
+//     username: req.cookies['username'],
+//     main: "Drip Drop",
+//   };
+
+//   res.render("dripDrop", templateVars);
+// });
 
 // SHOW SHORT URLS //
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  const templateVars = { 
+  const templateVars = {
+    username: req.cookies["username"],
     id: id,
-     longURL: urlDatabase[id] 
-    };
+    longURL: urlDatabase[id],
+  };
 
   res.render("urls_show", templateVars);
 });
 
-app.get('*', (req, res) => {
-  res.render('404');
-})
+app.get("*", (req, res) => {
+  res.render("404");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
